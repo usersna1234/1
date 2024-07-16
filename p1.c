8f40167cb6b6ec5ee61394130f8cabb768c56c93
Anil Kumar V Kali <136366929+anil34k@users.noreply.github.com>
2024-07-16 22:45:59 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 int binarySearch(int arr[], int left, int right, int target);
-int main() {
-    int n, target;
-    printf("Enter number of elements and sorted integers:\n");
+void main() {
+    int n, i, target;
+    printf("Enter number of elements:\n");
     scanf("%d", &n);
     int arr[n];
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printf("Enter the sorted array:\n");
+    for (i = 0; i < n; i++) 
+        scanf("%d", &arr[i]);
     printf("Enter element to search for: ");
     scanf("%d", &target);
     int index = binarySearch(arr, 0, n - 1, target);
     if (index != -1) 
-    printf("Element %d found at index %d.\n", target, index);
-    else printf("Element %d not found.\n", target);
-    return 0;
+        printf("Element %d found at index %d.\n", target, index);
+    else 
+        printf("Element %d not found.\n", target);
 }
 int binarySearch(int arr[], int left, int right, int target) {
     if (left > right) 
-    return -1;
+        return -1;
     int mid = left + (right - left) / 2;
     if (arr[mid] == target) 
-    return mid;
+        return mid;
     else if (arr[mid] > target) 
-    return binarySearch(arr, left, mid - 1, target);
-    else return binarySearch(arr, mid + 1, right, target);
+        return binarySearch(arr, left, mid - 1, target);
+    else 
+        return binarySearch(arr, mid + 1, right, target);
 }
